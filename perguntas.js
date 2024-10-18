c95aa6568fb3cc5bcd93b092e784ed7ebd25a279
thayeme <161500169+thayeme@users.noreply.github.com>
2024-10-18 15:15:59 -0300

    hbf

---
+++ perguntas.js
@@ -0,0 +1,9 @@
+criaCartao(
+    "Cuidados para animais",
+    "oque não dar para meu animal?",
+    "Leite e derivados,Chocolate e doces,Cebola e alho,Uvas e passas,Café e chá preto,Frutas cítricas,Doces dietéticos,Abacate,Tomate e batata,Noz macadâmia, Massas cruas de pão ou bolo,Ossos de aves cozidos,Frituras e alimentos gordurosos
+)
+
+criaCartao(
+    
+)
